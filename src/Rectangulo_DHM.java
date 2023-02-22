cf4835d5d9ed234f20b2e35a08e0e4032cc1917d
David <davidh@severoochoa.es>
2023-02-22 12:57:41 +0100

    versión comentada

--- src/Rectangulo_DHM.java
+++ src/Rectangulo_DHM.java
@@ -1,19 +1,38 @@
-
+/**
+ * 
+ * @author David Hernández
+ * @version 1.2
+ *
+ */
 public class Rectangulo_DHM extends FiguraGeometrica_DHM {
 	private double l1;
 	private double l2;
 	
+	/**
+	 * Constructor de clase Rectangulo_DHM
+	 * @param tipoFigura
+	 * @param lG
+	 * @param lP
+	 */
 	public Rectangulo_DHM(String tipoFigura, double lG, double lP) {
 		super(tipoFigura);
 		l1 = lG;
 		l2 = lP;
 	}
 
+	/**
+	 * Devuelve el area
+	 * @return area
+	 */
 	@Override
 	public double area() {
 		return l1 * l2;
 	}
 	
+	/**
+	 * Devuelve el perimetro
+	 * @return perimetro
+	 */
 	@Override
 	public double perimetro() {
 		return (2 * l1) + (2 * l2);
